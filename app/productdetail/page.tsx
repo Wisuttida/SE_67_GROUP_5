0d20590467e4368ab7583d6a04bb9bcea7d4a1cc
KU3495 <sorrawit.sas@ku.th>
2025-02-16 16:01:21 +0700

    product detail update

--- app/productdetail/page.tsx
+++ app/productdetail/page.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Navbar from '@/components/Navbar'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
 const ProductdetailPage = () => {
   return (
-    <div>page Add</div>
+    <div>
+      {/* Navbar */}
+      <Navbar />
+
+
+      {/* Main Content */}
+      <main className="max-w-7xl mx-auto p-6">
+
+        {/* Product Showcase (รายการสินค้าตัวอย่าง) */}
+          <div className="bg-gray-100 p-4 rounded-lg shadow-lg">
+            <section className="grid grid-cols-2 gap-8">
+            
+              <img src="/path/to/product1.jpg" alt="Product 1" className="w-full h-48 object-cover rounded-lg" />
+              
+              <section className="grid grid-rows-2 gap-8">
+                <div>
+
+                <h2 className="text-xl font-semibold mt-4">Product 1</h2>
+                <p className="text-gray-700 mt-2">$99.99</p>
+                </div>
+                <Button variant="default" className="mt-4 w-full">Add to Cart</Button>
+              </section>
+              
+            
+            </section>
+          </div>
+
+      </main>
+
+    </div>
   )
 }
 
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Banner() {
   return (
-    <div className="bg-gray-100 p-4 rounded-lg shadow-lg">
-        <img src="/path/to/product1.jpg" alt="Product 1" className="w-full h-48 object-cover rounded-lg" />
+    <div className="flex justify-center items-center">
+        <div className="bg-gray-100 p-4 rounded-lg shadow-lg w-10/12">
+            <img src="/path/to/product1.jpg" alt="Product 1" className="w-full h-48 object-cover rounded-lg" />
+        </div>
     </div>
   )
 }
