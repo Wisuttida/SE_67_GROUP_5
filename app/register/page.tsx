9122a198f8bedbe3c893921978f58fce8970d690
KU3495 <sorrawit.sas@ku.th>
2025-02-28 22:10:50 +0700

    edit ignor

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import {Card,CardContent,CardFooter,CardHeader,CardTitle,} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
